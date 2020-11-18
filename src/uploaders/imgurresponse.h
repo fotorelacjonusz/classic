6b8279cb5a25c227b11171b08ba2a5283874da79
Sebastian Skałacki <skalee@gmail.com>
2020-11-18 18:38:53 +0100

    Write some documentation for ImgurResponse class

--- src/uploaders/imgurresponse.h
+++ src/uploaders/imgurresponse.h
@@ -5,6 +5,23 @@
 
 class NetworkTransaction;
 
+/**
+ * @brief Parsed JSON responses from Imgur.
+ *
+ * Imgur API returns JSON objects, which conform following schema:
+ *
+ *   {
+ *     "success": <bool, whether the call has succeeded>,
+ *     "status": <integer number equal to HTTP status code of the response>,
+ *     "data": <object with more details>
+ *   }
+ *
+ * The "data" is always an object and contains more details about outcome of
+ * given operation, most notably "error", which contains error message
+ * in case of error responses.
+ *
+ * Call debug() to print the response in human-readable way in development.
+ */
 class ImgurResponse
 {
 	void parseResponse(const NetworkTransaction &tr);
@@ -13,7 +30,8 @@ public:
 	ImgurResponse(const NetworkTransaction &tr);
 	void debug() const;
 
-	// TODO: Make it <QString, QVariant> to avoid unnecessary data conversions.
+	/// @brief Key-value pairs found in "data" object of server's response.
+	/// @todo Consider making it <QString, QVariant> to avoid type conversions.
 	QMap<QString, QString> data;
 	bool success;
 	int status;
